6d2ca096da2f20740d07c1342844215f8a373524
DUOLabs333 <dvdugo333@gmail.com>
2023-10-20 23:02:48 -0400

    Get some more done

--- main.go
+++ main.go
@@ -18,6 +18,13 @@ import (
 	"syscall"
 )
 
+func runCommand(args... string){
+	command:=exec.Command(args[0],args[1:]...)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Run()
+}
+
 type OverlayFS struct {
 	paths []string
 	modes []string
@@ -54,7 +61,7 @@ func NewFS(options []string, mountpoint string) (OverlayFS){
 			split_index=strings.LastIndex(value,"=")
 		}
 		result.paths=append(result.paths,value[:split_index])
-		result.modes=append(result.paths,value[split_index+1:])
+		result.modes=append(result.modes,value[split_index+1:])
 	}
 	
 	return result
@@ -85,18 +92,29 @@ func (fs OverlayFS) Join(elem ...string) string{
 }
 
 func (fs OverlayFS) ReadDir(path string) ([]os.FileInfo, error){
-	fmt.Println("Dir: "+path)
+	fmt.Println("Dir:",path)
+	
 	dirs:=fs.joinRelative(path)
+	dirMap:=make(map[string]os.FileInfo)
 	result:=make([]os.FileInfo,0)
 	
+	//If there's A/C and B/C, take A/C
 	for _, dir :=range dirs{
 		dirEntries,_:=os.ReadDir(dir)
 		for _, entry:= range dirEntries{
-			info,_:=entry.Info()
-			result=append(result,info)
+			_,ok:=dirMap[entry.Name()]
+			if ok{
+				continue
+			}else{
+				info, _:=entry.Info()
+				dirMap[entry.Name()]=info
+			}
 		}
 	}
-	fmt.Println("Files:",result)
+	
+	for _,info := range dirMap{
+		result=append(result,info)
+	}
 	return result,nil
 }
 
@@ -111,52 +129,73 @@ func (fs OverlayFS) Lstat(filename string) (os.FileInfo, error){
 	return os.Lstat(fs.findFirstExisting(filename))
 }
 
-func (fs OverlayFS) Open(filename string) (billy.File, error){
-	fmt.Println("Open:",filename)
-	open,err:=fs.OpenFile(fs.findFirstExisting(filename),os.O_RDONLY,0)
-	return &OverlayFile{open},err
+func (fs OverlayFS) Chtimes(name string, atime time.Time, mtime time.Time) error{
+	fmt.Println("Chtimes:",name)
+	return os.Chtimes(fs.findFirstExisting(name),atime,mtime)
 }
 
-//If O_RDONLY, O_WRONLY, or O_RDWR is specified and RO directory, then replace with O_RDONLY
-//If O_CREATE is specified
+func (fs OverlayFS) Open(filename string) (billy.File, error){
+	fmt.Println("Open:",filename)
+	return fs.OpenFile(filename,os.O_RDONLY,0)
+}
+
+func (fs OverlayFS) Create(filename string) (billy.File, error){
+	fmt.Println("Create:",filename)
+	return fs.OpenFile(filename,os.O_CREATE | os.O_TRUNC,0666)
+}
 
 /* If filename in deleted:
 If O_CREATE:
-	if file is successfully created, remove from deleted (not neccessarily delete folders/file, but change created time to 1)
+	if file exists at the end, remove from deleted (not neccessarily delete folders/file, but change created time to 1)
 Else:
 	Return notexist
 */
 func (fs OverlayFS) OpenFile(filename string, flag int, perm os.FileMode) (billy.File, error){
 	original_filename:=filename
 	
-	temp_filename:=fs.findFirstExisting(filename)
-	_, err:=os.Stat(temp_filename)
+	fmt.Println("Openfile:",original_filename)
+	
+	possible_filename:=fs.findFirstExisting(original_filename)
+	_, err:=os.Stat(possible_filename)
 	if !os.IsNotExist(err){
-		filename=temp_filename
+		filename=possible_filename
 	}else{
-		if (flag & os.O_CREATE){
+		if (flag & os.O_CREATE == os.O_CREATE){
 			allRO:=true
 			for i,_ := range fs.paths{
+				fmt.Println(fs.modes[i])
 				if fs.modes[i]=="RW"{
-					filename=os.Join(fs.ppaths[i],filename)
+					filename=path.Join(fs.paths[i],original_filename)
 					allRO=false
 					break
 				}
 			}
 			if allRO{
-				return billy.File{}, os.ErrPermission
+				return *new(billy.File), os.ErrPermission
 			}
 		}
+	}
 	inRO:=false
 	for i,_:= range fs.paths{
+		if path.Join(fs.paths[i],original_filename)==filename{
+			if fs.modes[i]=="RO"{
+				inRO=true
+			}
+			break
+		}
+	}
+	
+	if inRO{
+		fmt.Println("Testing")
+		flag=flag & ^(os.O_RDONLY | os.O_RDWR | os.O_WRONLY)
+		flag |= os.O_RDONLY
+	}
 		
-fmt.Println("Openfile:",filename)
-open,err:=os.OpenFile(fs.findFirstExisting(filename),flag,perm)
-return &OverlayFile{open},err
+		
+	open,err:=os.OpenFile(filename,flag,perm)
+	return &OverlayFile{open},err
 }
 
-
-//Create --- findFirstexisting if the file exists, return error already exists. Else, create in the first RW folder. If no RW directory, return RO system
 
 func runServer(options []string,mountpoint string){
 	listener, err := net.Listen("tcp", ":10000") //Later, use port that's defined in main as argument to function
@@ -200,15 +239,13 @@ for {
 	}
 }
 
-mount_command:=exec.Command("sudo","mount", "-t", "nfs", "-oport=10000,mountport=10000,vers=3,tcp,noacl","-vvv","127.0.0.1:/", mountpoint)
-mount_command.Stdout = os.Stdout
-mount_command.Stderr = os.Stderr
-mount_command.Run()
+runCommand("mkdir","
+runCommand("sudo","mount", "-t", "nfs", "-oport=10000,mountport=10000,vers=3,tcp,noacl","-vvv","127.0.0.1:/", mountpoint)
+
 
 <-done
-unmount_command:=exec.Command("sudo","umount", mountpoint)
-unmount_command.Stdout = os.Stdout
-unmount_command.Stderr = os.Stderr
-unmount_command.Run()
-}
+
 //wait until SIGTERM or SIGINT, then unmount
+runCommand("sudo","umount", mountpoint)
+fmt.Println()
+}
