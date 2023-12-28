2e8d64024f6ba07f0542dfa2b593a30761c9eafe
DUOLabs333 <dvdugo333@gmail.com>
2023-12-27 23:55:06 -0500

    Remove some unneeded debugging statements

--- main.go
+++ main.go
@@ -193,17 +193,10 @@ func (fs OverlayFS) OpenFile(filename string, flag int, perm os.FileMode) (billy
 		defer fs.createErrorCheck(filename, original_filename)
 	}
 	
-	tempStat,err:=fs.Lstat(original_filename)
-	readLink,_:=fs.Readlink(original_filename)
-	if err==nil && (tempStat.Mode() & os.ModeSymlink !=0){
-		fmt.Println("Symlink1: %s",original_filename)
-		fmt.Println("Maps to: %s", readLink)
-	}
-	
 	open,err:=os.OpenFile(filename,flag,perm)
 	
 	fmt.Println("Openfile finished!")
-	return &OverlayFile{open},err
+	return &OverlayFile{open},err //Remember to override Name to output the original_filename, not the underlying filename
 }
 
 func (fs OverlayFS) Remove(filename string) error{
